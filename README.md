05ae8350d845aa51b76e2457b081fc72998f8644
Mark Vartanyan <kolypto@gmail.com>
2020-07-24 23:48:58 +0300

    Project template

---
+++ README.md
@@ -0,0 +1,2 @@
+![Tests](https://github.com/kolypto/py-sa2schema/workflows/Tests/badge.svg)
+[![Pythons](https://img.shields.io/badge/python-3.7%E2%80%933.8-blue.svg)](noxfile.py)
---
+++ noxfile.py
@@ -0,0 +1,50 @@
+import nox.sessions
+
+
+nox.options.reuse_existing_virtualenvs = True
+nox.options.sessions = ['tests', 'tests_pydantic', 'tests_sqlalchemy']
+
+
+@nox.session(python=['3.8'])
+def tests(session: nox.sessions.Session, sqlalchemy=None, pydantic=None):
+    """ Run all tests """
+    session.run('poetry', 'install')
+    session.install('.')
+
+    # Specific versions
+    if sqlalchemy:
+        session.install(f'sqlalchemy=={sqlalchemy}')
+    if pydantic:
+        session.install(f'pydantic=={pydantic}')
+
+    session.run('pytest', 'tests/')
+
+
+@nox.session(python='3.8')
+@nox.parametrize(
+    'pydantic',
+    [
+        '1.0',
+        '1.1', '1.1.1',
+        '1.2',
+        '1.3',
+        '1.4',
+        '1.5', '1.5.1',
+        '1.6', '1.6.1',
+    ]
+)
+def tests_pydantic(session, pydantic):
+    tests(session, pydantic=pydantic)
+
+
+
+@nox.session(python=['3.8'])
+@nox.parametrize(
+    'sqlalchemy',
+    [
+        *(f'1.3.{x}' for x in range(0, 1+18)),
+    ]
+)
+def tests_sqlalchemy(session, sqlalchemy):
+    tests(session, sqlalchemy=sqlalchemy)
+
